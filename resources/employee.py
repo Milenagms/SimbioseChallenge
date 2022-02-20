9016d84293726337384805f35c84d81e8a0b6686
Milena <milena.gomes@dremio.com>
2022-02-20 13:49:31 -0300

    employee - POST E GET

--- resources/employee.py
+++ resources/employee.py
@@ -2,30 +2,30 @@ from flask_restful import Resource
 from flask import request, jsonify
 from sql_alchemy import project_base
 
-employees = [
-    {'register_number': 1,
-     'full_name': 'Milena Ferreira Gomes',
-     'CPF': '999.999.999.99'
-     },
-    {'register_number': 2,
-     'full_name': 'Douglas Viana',
-     'CPF': '123.999.999.99'
-     },
-    {'register_number': 4,
-     'full_name': 'Roberto Paolo',
-     'CPF': '999.123.999.99'
-     },
-    {'register_number': 3,
-     'full_name': 'Bruno Sena',
-     'CPF': '123.456.789.45'
-     },
-]
+
+class ModelEmployee(project_base.Model):
+    __tablename__ = 'employess'
+    register_number = project_base.Column(project_base.Integer, primary_key=True)
+    full_name = project_base.Column(project_base.String(80))
+    cpf = project_base.Column(project_base.Integer)
+
+    def __init__(self, register_number, full_name, cpf):
+        self.register_number = register_number
+        self.full_name = full_name
+        self.cpf = cpf
+
+    def formatted_data(self):
+        return {
+            "Número de registro": self.register_number,
+            "Nome completo": self.full_name,
+            "CPF": self.cpf
+        }
 
 
 class Employees(Resource):
     @staticmethod
     def get():
-        return {'Employees': employees}
+        return {'Teams da Simbiose cadastrado': [employee.formatted_data() for employee in ModelEmployee.query.all()]}
 
 class Employee(Resource):
     def get(self, register_number):
@@ -36,10 +36,16 @@ class Employee(Resource):
         return {'message': 'Desculpa, esse funcionário não se encontra na nossa base de dados'}
 
     def post(self, register_number):
-        request_data = request.get_json()
-        return jsonify(request_data)
+        #  TODO criar uma verificação para quando já estiver criado;
+        all_value = [request.json['full_name'], request.json['cpf']]
+        print(all_value)
+        new_employee = ModelEmployee(register_number, *all_value)
+        project_base.session.add(new_employee)
+        project_base.session.commit()
+        return jsonify(request.json)
 
     def delete(self, register_number):
+        # TODO pensar em um jeito de fazer isso e que funcione da proxima vez
         global employees
         employees = [employee for employee in employees if employee['register_number'] != register_number]
         return{'message': 'employee deleted'}
