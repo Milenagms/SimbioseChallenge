d6b028853d0726ed64c3958f74bd9e45273e3663
Milena <milena.gomes@dremio.com>
2022-02-20 09:16:42 -0300

    team - post sem verificações

--- resources/team.py
+++ resources/team.py
@@ -29,15 +29,14 @@ teams = [
 class Teams(Resource):
     @staticmethod
     def get():
-        return {'Teams': teams}
+        return {'Teams da Simbiose cadastrado': [team.json() for team in ModelTeam.query.all()]} # SELECT * FROM teams
 
 
 class Team(Resource):
-
     def post(self, team_id):
         team_name = request.json['team_name']
 
-        new_team = ModelTeam(team_name=team_name)
+        new_team = ModelTeam(team_id, team_name=team_name)
 
         project_base.session.add(new_team)
         project_base.session.commit()
@@ -45,7 +44,7 @@ class Team(Resource):
         return jsonify(request.json)
 
     def get(self, team_id):
-        return {'teams': [{"team_id": team.team_id, "team_name": team.team_name} for team in ModelTeam.query.all()]}
+       pass
 
 
 class ModelTeam(project_base.Model):
@@ -53,3 +52,12 @@ class ModelTeam(project_base.Model):
     team_id = project_base.Column(project_base.Integer, primary_key=True)
     team_name = project_base.Column(project_base.String)
 
+    def __init__(self, team_id, team_name):
+        self.team_id = team_id
+        self.team_name = team_name
+
+    def json(self):
+        return {
+            'Registro do time': self.team_id,
+            'Nome da equipe': self.team_name
+        }
